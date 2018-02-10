d086a5c02f27c064cc9a34be1a183abf5e61ac11
Kevin Franklin <kevin@whitehat.local>
2018-02-10 14:30:03 -0500

    Added the normalization class

--- electionBuster.py
+++ electionBuster.py
@@ -23,6 +23,34 @@ import socket
 from datetime import date
 import urllib 
 from multiprocessing import Pool as ThreadPool 
+import collections
+import csv
+import operator
+
+class NameDenormalizer(object):
+	def __init__(self, filename=None):
+		filename = filename or 'names.csv'
+		lookup = collections.defaultdict(list)
+		with open(filename) as f:
+			reader = csv.reader(f)
+			for line in reader:
+				matches = set(line)
+			for match in matches:
+				lookup[match].append(matches)
+		self.lookup = lookup
+	def __getitem__(self, name):
+		name = name.upper()
+		if name not in self.lookup:
+			raise KeyError(name)
+			names = reduce(operator.or_, self.lookup[name])
+		return names
+
+	def get(self, name, default=None):
+		try:
+			return self[name]
+		except KeyError:
+			return set( [name] )
+
 
 # Program Timer
 start_time = time.time()
