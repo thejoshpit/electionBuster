a36d08d7f97e07c010e24f5afe346e487e0a92eb
Kevin Franklin <kevin@whitehat.local>
2018-02-26 06:53:51 -0500

    Added response code from origional output of electionBuster.py to output of getips.sh

--- getips.sh
+++ getips.sh
@@ -5,18 +5,19 @@ resultfile=$1
 procid=$$
 tmpfile=/tmp/$procid.txt
 
-grep -A 1 "Page Exist" $resultfile  | paste - - - | awk '{ print $3, $4}' | sed 's/,$//' > $tmpfile
+grep -A 1 "Page Exist" $resultfile  | paste - - - | awk '{ print $3, $4, $5}' | sed 's/,$//' > $tmpfile
 echo $tmpfile
 while read j
 do
 	arg1=`echo $j | awk '{ print $1}'`
 	arg1d=`echo "$arg1" | awk -F/ '{print $3}'`
+	argresp=`echo "$j" | awk '{print $3}'`
 	arg1ip=`dig +short $arg1d`
 
-	arg2=`echo $j | awk '{ print $2}'`
+	arg2=`echo $j | awk '{ print $2}' | tr -d ', '`
 	arg2d=`echo "$arg2" | awk -F/ '{print $3}'`
 	arg2ip=`dig +short $arg2d | tail -1`
-	echo $arg2 $arg1 $arg1ip | tr ' ' ','
+	echo $arg2 $argresp $arg1 $arg1ip | tr ' ' ','
 done <$tmpfile
 
 rm $tmpfile
