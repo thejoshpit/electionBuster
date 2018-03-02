54220523df816db163f8ed816d2059b3532540e6
Kevin Franklin <kevin@whitehat.local>
2018-03-02 03:59:02 -0500

    Simplified the race name

--- graph/graphRace.sh
+++ graph/graphRace.sh
@@ -17,11 +17,6 @@ done | awk '{ print $1, $4, $5 }' | sed 's/,$/\//' > $tmpfile
 dte=`pwd`
 gdtd=`basename $dte`
 
-cd ..
-
-nme=`pwd`
-name=`basename $nme`_
-name2=`basename $nme`
 cd $cur
 
 # Identify Strong relationships
@@ -32,14 +27,15 @@ $EBHOME/graph/find_weak.pl $tmpfile | sort | uniq > weak.txt
 
 # We could graph both together in one go but this is simpler 
 graphTitle="Candidate Name Space Planting or Redirection"
-$EBHOME/graph/network_graph.r -f strong.txt -t "$graphTitle" -d "$name2 $gdtd"
+$EBHOME/graph/network_graph.r -f strong.txt -t "$graphTitle" -d "$2 $gdtd"
 graphTitle="Bad Faith Domain or Cyber Squatting"
-$EBHOME/graph/network_graph.r -f weak.txt -t "$graphTitle" -d "$name2 $gdtd"
+$EBHOME/graph/network_graph.r -f weak.txt -t "$graphTitle" -d "$2 $gdtd"
 
 # Combine the pdf's into a single pdf
 pdftk weak.txt.pdf strong.txt.pdf cat output t1.pdf
 # Update some meta information
 pdftk t1.pdf update_info $EBHOME/graph/PDFINFO1.TXT output t2.pdf
+name=$2_
 pdftk t2.pdf update_info $EBHOME/graph/PDFINFO2.TXT output $name$gdtd.pdf
 echo "Output graph: $name$gdtd.pdf"
 # Clean up
