2c9f6c5ffb6b842e893938db7179305227e84520
Kevin Franklin <kevin@whitehat.local>
2018-03-02 03:58:12 -0500

    Added namey

--- grabimages.sh
+++ grabimages.sh
@@ -11,5 +11,19 @@ cd $d
 for j in `ls result*.txt` 
 do
 	nohup env DISPLAY=:0 $homed/resultsScreen.sh $j &
+	outf=`basename $j .txt`
+	$homed/getips.sh $j > $outf.ip
 done
+
+mkdir ~/git2/electionBuster/president/$d
+cp *.ip ~/git2/electionBuster/president/$d
+
+
+$homed/graph/graphRace.sh . President
+cp *.pdf ~/git2/electionBuster/president/$d
+
+cd ~/git2/electionBuster/
+git add president/$d
+git commit -m "ElectionBuster results $d" president/$d
+git push origin master 
 
